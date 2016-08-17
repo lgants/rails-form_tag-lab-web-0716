2b5030419ebf338831674d0bf64416ba5f00ed11
lgants <lgants@gmail.com>
2016-08-17 18:38:52 -0400

    rails form tab lab complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:first_name,:last_name)
+    end
 end
